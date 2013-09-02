63720a6d7820cabe55c008288293f39c958e22f4
lcplcy <lcp_lcy@hotmail.com>
2013-09-02 17:57:37 +0800

    Updated index.php and new login.php

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Fabrica | Making Things Happen</title>
+  <link rel = "stylesheet" href = "css/bootstrap.css" media="screen">
+	<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ 	<div class="navbar navbar-inverse navbar-fixed-top">
+ 		<div class="navbar-inner">
+ 			<div class="container">
+ 				<a class="brand" href="#">Fabrica</a>
+ 				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li><a href="#">Kids for Kids</a></li>
+						<li><a href="#">Events/Occasions</a></li>
+						<li><a href="#">Educational Models</a></li>
+						<li><a href="#">Corporate Gifts</a></li>
+					</ul>
+					<ul class="nav pull-right">
+					<li><a href="login.php">Register/Login</a></li>
+					</ul>
+				</div>
+ 			</div>
+ 		</div>
+ 	</div>
  </body>
 </html>
---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Fabrica | Login/Register</title>
+  <link rel = "stylesheet" href = "css/bootstrap.css" media="screen">
+	<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+ </head>
+ <body>
+ 	<div class="container">
+ 		<form class="form-signin"></form>
+ 	</div>
+ </body>
